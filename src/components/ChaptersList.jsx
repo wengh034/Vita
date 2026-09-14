2bf6ef5cf8ffa35890b0313d2c9a13f668bf5613
wengh034 <acostabritosderlisdavid@gmail.com>
2026-09-13 23:29:33 -0300

    fix mobile streak3.1

--- src/components/ChaptersList.jsx
+++ src/components/ChaptersList.jsx
@@ -284,7 +284,7 @@ if (loading) {
         padding: "0.5rem",
         boxSizing: "border-box",
         minHeight: "489px",
-        contain:"layout style"
+        contain:"layout"
       }}
     >
       <div
